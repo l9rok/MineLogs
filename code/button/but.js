39379a7aeed6a2a80bd7bd7e10f8c1198268d8ef
Marat Toma <61556722+l9rok@users.noreply.github.com>
2024-08-15 17:28:33 +0300

    Delete code directory

--- code/button/but.js
+++
@@ -1,8 +0,0 @@
-btnElement = document.querySelector(".btn");
-resElement = document.querySelector(".res");
-finum = document.querySelector('[name="finum"]');
-senum = document.querySelector('[name="senum"]');
-
-btnElement.addEventListener("click", function(){
-    resElement.textContent = parseInt(finum.value) * parseInt(senum.value);
-});
